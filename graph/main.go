1ba1e7df9bdc4c4d579efed9845d052e184e4fb8
takuya <nagataku021@outlook.jp>
2021-08-28 18:53:59 +0900

    grpc用の設定を追加

--- graph/main.go
+++ graph/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
-	"graph_paradise/api"
+	"context"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
-	"os"
+
 	// "github.com/jinzhu/gorm"
+	"graph_paradise/grpc/user"
 )
 
 func main() {
-	dir, _ := os.Getwd()
+	// dir, _ := os.Getwd()
 
-	http.HandleFunc("/graph", graph)
-	http.HandleFunc("/getRooms", api.GetRooms)
-	http.HandleFunc("/getAreas", api.GetAreas)
-	http.HandleFunc("/getDataTypeList", api.GetDataTypeList)
-	http.HandleFunc("/getDates", api.GetDates)
-	http.HandleFunc("/getData1", api.GetData1)
-	http.HandleFunc("/GetDataForTable", api.GetDataForTable)
-	http.HandleFunc("/getDataForDaily", api.GetDataForDaily)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/"))))
+	// http.HandleFunc("/graph", graph)
+	// http.HandleFunc("/getRooms", api.GetRooms)
+	// http.HandleFunc("/getAreas", api.GetAreas)
+	// http.HandleFunc("/getDataTypeList", api.GetDataTypeList)
+	// http.HandleFunc("/getDates", api.GetDates)
+	// http.HandleFunc("/getData1", api.GetData1)
+	// http.HandleFunc("/GetDataForTable", api.GetDataForTable)
+	// http.HandleFunc("/getDataForDaily", api.GetDataForDaily)
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/"))))
 	// port
-	http.ListenAndServe(":80", nil)
+	// http.ListenAndServe(":80", nil)
+	conn, err := grpc.Dial("127.0.0.1:90", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("client connection error:", err)
+	}
+	defer conn.Close()
+	client := user.NewCatClient(conn)
+	message := &user.GetName{User: "takuya"}
+	res, err := client.GetMyCat(context.TODO(), message)
+	fmt.Printf("result:%#v \n", res)
+	fmt.Printf("error::%#v \n", err)
 }
 
 func graph(w http.ResponseWriter, r *http.Request) {
